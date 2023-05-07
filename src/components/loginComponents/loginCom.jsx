2b7e8ba953d73f23dd1a43581af26f98786aa118
shibuambalathara <shibu@autobse.com>
2023-05-07 08:34:31 +0530

    remove forgot password

--- src/components/loginComponents/loginCom.jsx
+++ src/components/loginComponents/loginCom.jsx
@@ -81,7 +81,7 @@ else{
                             href="#"
                             className="text-xs text-purple-600 hover:underline"
                         >
-                            Forget Password?
+                            {/* Forget Password? */}
                         </a>
                         <div className="mt-6">
                             <button type='submit' className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
