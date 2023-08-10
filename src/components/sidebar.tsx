cf5702b0b1966f4455952273e45c94c2416f0703
shibuambalathara <shibu@autobse.com>
2023-08-10 10:50:08 +0530

    we can upload multiple image at a time and get urls

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -19,7 +19,7 @@ const Sidebar = () => {
       {Sidebar_items.map((item)=>{
         return(
           <>{item.path===pathNm?
-          <div><button onClick={() => HandleClick(item.path)}  className='btn bg-white text-start font-bold text-orange-500  w-full '> {item.name}</button>  </div>
+          <div><button onClick={() => HandleClick(item.path)}  className='btn  text-start font-bold text-red-500  w-full '> {item.name}</button>  </div>
           :<div><button onClick={() => HandleClick(item.path)}  className='btn btn-ghost  text-start font-bold   w-full text-white'> {item.name}</button>  </div>
       }
           </>
